a65284724bc1f597db5605345fc79bcfaea25883
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-09-18 08:48:22 -0700

    functional code

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,43 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
+  const [uploadDuration, setUploadDuration] = useState<number | null>(null);  // Store upload duration
 
   const handleFileChange = (event: any) => {
-    setFile(event.target.files[0]);
+    // Convert the FileList to an array and set the state
+    setFiles(Array.from(event.target.files));
   };
 
-  const uploadFile = async (file: any) => {
-    // console.log('UPLOADING FILE NOW......', file.name)
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('filename', file.name.replace(/\s/g, ''));
 
-    try {
-      await fetch('/api/upload', {
+  const uploadFiles = async () => {
+    const startTime = Date.now();  // Timestamp before starting uploads
+
+    const uploadPromises = files?.map(async (file) => {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('filename', file.name.replace(/\s/g, ''));
+
+      return fetch('/api/upload', {
         method: 'POST',
         body: formData
       });
-    } catch (error) {
-      console.log('error', error)
-    }
+    });
 
+    try {
+      await Promise.all(uploadPromises);
+      const endTime = Date.now();  // Timestamp after all uploads are finished
+      setUploadDuration(endTime - startTime);  // Set the duration difference
+    } catch (error) {
+      console.log('error', error);
+    }
   };
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={() => uploadFile(file as File)}>Upload to Azure</button>
+      <input multiple type="file" onChange={handleFileChange} />
+      <button onClick={uploadFiles}>Upload to Azure</button>
+      {uploadDuration && <p>Upload duration: {uploadDuration} milliseconds</p>}
     </div>
   );
 }
