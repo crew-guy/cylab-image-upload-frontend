20d47781dd0e0568dccebc7fd86711e20431546a
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-10-04 00:04:25 -0700

    basic tests passing

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,9 +1,25 @@
 import { render, screen } from '@testing-library/react'
 import App from '../app/page';
-import exp from 'constants';
 
-it('should have Image text', () => {
-    render(<App />) //ARRANGE
-    const linkElement = screen.getByText(/Image Uploader/i) //ACT
-    expect(linkElement).toBeInTheDocument() //ASSERT
+
+describe('Home', () => {
+    it('should have Image text', () => {
+        render(<App />) //ARRANGE
+        const linkElement = screen.getByText(/Image/i) //ACT
+        expect(linkElement).toBeInTheDocument() //ASSERT
+    })
+    it('should have Uploader text', () => {
+        render(<App />) //ARRANGE
+        const linkElement = screen.getByText(/Uploader/i) //ACT
+        expect(linkElement).toBeInTheDocument() //ASSERT
+    })
+    it('should have "Image Uploader" text', () => {
+        render(<App />) //ARRANGE
+        const linkElement = screen.getByRole('heading', {
+            name: 'Image Uploader'
+        }) //ACT
+        expect(linkElement).toBeInTheDocument() //ASSERT
+    })
+
+
 })
